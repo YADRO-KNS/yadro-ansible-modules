3444aaf2c82271c3a573992cb6d4d74bd1c9393c
r.safin <r.safin@yadro.com>
2021-10-19 22:07:45 +0300

    Add calls check to bmc_firmware_info test

--- tests/unit/plugins/modules/test_bmc_firmware_info.py
+++ tests/unit/plugins/modules/test_bmc_firmware_info.py
@@ -22,21 +22,20 @@ class TestBmcFirmwareInfo(ModuleTestCase):
     module = bmc_firmware_info
 
     def test_firmware_info_reading(self, mocker, module_args):
+        firmware_id = "Image Id"
+        firmware_info = {"Id": firmware_id, "Description": "Description"}
         client_mock = MagicMock()
-        client_mock.get_manager.return_value = {
-            "Name": "BMC",
-            "Links": {
-                "ActiveSoftwareImage": "Image",
-            }
-        }
-        firmware_info = {"Name": "Image", "Description": "Description"}
+        client_mock.get_manager.return_value = {"Links": {"ActiveSoftwareImage": firmware_id}}
         client_mock.get_software_inventory.return_value = firmware_info
-
         mocker.patch("{0}.create_client".format(self.module.__name__), return_value=client_mock)
+
         expected_json = {
             "msg": "Firmware information successfully read.",
             "changed": False,
             "firmware_info": firmware_info,
         }
         result = self.run_module_expect_exit_json(module_args)
+
+        client_mock.get_manager.assert_called()
+        client_mock.get_software_inventory.assert_called_with(firmware_id)
         assert result == expected_json
