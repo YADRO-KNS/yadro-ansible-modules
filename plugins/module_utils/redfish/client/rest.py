46198ea29cde2a559a0ceebcd1b59bab5a66edc1
skovalev <s.kovalev@yadro.com>
2022-04-26 19:10:38 +0300

    [INFSPB-2117] processing 401 code
    When 401 code is received, RESTClient throws RESTClientUnauthorized exception

--- plugins/module_utils/redfish/client/rest.py
+++ plugins/module_utils/redfish/client/rest.py
@@ -24,6 +24,7 @@ from ansible_collections.yadro.obmc.plugins.module_utils.redfish.client.exceptio
     RESTClientRequestError,
     RESTClientNotFoundError,
     RESTClientConnectionError,
+    RESTClientUnauthorized,
 )
 
 
@@ -39,7 +40,10 @@ class RESTClient(HTTPClient):
         except HTTPError as e:
             if e.code == 404:
                 raise RESTClientNotFoundError("Not found: {0}".format(e.url))
+            elif e.code == 401:
+                raise RESTClientUnauthorized('Unauthorized error')
             else:
+                # TODO: Why json.load() is used? It is intended for files reading
                 raise RESTClientRequestError("Request finished with error: {0}".format(json.load(e)))
         except (URLError, SSLValidationError, ConnectionError) as e:
             raise RESTClientConnectionError("Cannot connect to server: {0}".format(str(e)))
