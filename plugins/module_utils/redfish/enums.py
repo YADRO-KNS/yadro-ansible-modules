e6c23cc6834801d985e84317baa3908475a9044d
skovalev <s.kovalev@yadro.com>
2022-05-27 11:47:07 +0300

    [INFSPB-2172] +module bmc_ldap_config

---
+++ plugins/module_utils/redfish/enums.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# YADRO OpenBmc Ansible Collection
+# Version 1.0.0
+# Copyright (c) 2021 YADRO (KNS Group LLC)
+
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from enum import Enum
+
+
+class AccountProvider(Enum):
+
+    OPENLDAP = 1
+    ACTIVE_DIRECTORY = 2
